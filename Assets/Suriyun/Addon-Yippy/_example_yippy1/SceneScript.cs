6b0c7f292e922c4e214e463fa7fbe85e330930b3
myzyryzm <ryanyoung3e@yahoo.com>
2021-10-21 12:00:50 -0700

    edited some suriyun

--- Assets/Suriyun/Addon-Yippy/_example_yippy1/SceneScript.cs
+++ Assets/Suriyun/Addon-Yippy/_example_yippy1/SceneScript.cs
@@ -4,55 +4,56 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Suriyun;
 
+namespace Suriyun
+{
+	public class SceneScript : MonoBehaviour {
 
-public class SceneScript : MonoBehaviour {
+		protected ControllerYippy yippy;
 
-	protected ControllerYippy yippy;
+		void Awake(){
+			Application.targetFrameRate = 120;
+		}
+			
 
-	void Awake(){
-		Application.targetFrameRate = 120;
-	}
-		
+		public void Reset(){
+			CheckYippy ();
+			yippy.gameObject.transform.position = new Vector3 (-1.12f,0.89f, 0f);
+		}
 
-	public void Reset(){
-		CheckYippy ();
-		yippy.gameObject.transform.position = new Vector3 (-1.12f,0.89f, 0f);
-	}
+		public virtual void ClearState(){
+			CheckYippy ();
+			yippy.ClearState ();
+		}
 
-	public virtual void ClearState(){
-		CheckYippy ();
-		yippy.ClearState ();
-	}
+		public virtual void Sit(){
+			CheckYippy ();
+			yippy.Sit ();
+		}
 
-	public virtual void Sit(){
-		CheckYippy ();
-		yippy.Sit ();
-	}
+		public virtual void Die(){
+			CheckYippy ();
+			yippy.Die ();
+		}
 
-	public virtual void Die(){
-		CheckYippy ();
-		yippy.Die ();
-	}
+		public virtual void Cheer(){
+			CheckYippy ();
+			yippy.Cheer ();
+		}
 
-	public virtual void Cheer(){
-		CheckYippy ();
-		yippy.Cheer ();
-	}
+		public virtual void Hit(){
+			CheckYippy ();
+			yippy.Hit ();
+		}
 
-	public virtual void Hit(){
-		CheckYippy ();
-		yippy.Hit ();
-	}
+		public virtual void SwitchAttackType(){
+			CheckYippy ();
+			yippy.SwitchAttackType ();
+		}
 
-	public virtual void SwitchAttackType(){
-		CheckYippy ();
-		yippy.SwitchAttackType ();
-	}
-
-	public void CheckYippy(){
-		if (yippy == null) {
-			yippy = GameObject.FindObjectOfType<ControllerYippy> ();
+		public void CheckYippy(){
+			if (yippy == null) {
+				yippy = GameObject.FindObjectOfType<ControllerYippy> ();
+			}
 		}
 	}
-
 }
