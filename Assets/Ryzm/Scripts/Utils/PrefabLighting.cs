d49630c461a58fa3bd654f9cd7579737c89d73fd
myzyryzm <ryanyoung3e@yahoo.com>
2021-10-24 09:38:54 -0700

    more stuff

---
+++ Assets/Ryzm/Scripts/Utils/PrefabLighting.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ryzm
+{
+    public class PrefabLighting : MonoBehaviour
+    {
+        public UnityEngine.Rendering.AmbientMode ambientMode = UnityEngine.Rendering.AmbientMode.Trilight;
+        public Color skyColor;
+        public Color equatorColor;
+        public Color groundColor;
+    }
+}
